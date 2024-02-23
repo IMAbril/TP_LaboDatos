977cab487d06f04de9a6eae0c5aae93b60cc797f
Rocio Dominguez <132227093+rocio2403@users.noreply.github.com>
2024-02-22 22:00:58 -0300

    subo Script para realizar las consultas

---
+++ Código/consultasSQL.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Trabajo Práctico N°1
+laboratorio de Datos - Verano 2024
+Autores :
+Nombre del Grupo: The Beatles 
+Fecha : Febrero 2024
+"""
+import DataFrames as data
+from inline_sql import sql, sql_val 
+
+sedes = data.sedes_df
+
+redesSociales = data.redSocial_df
+
+secciones = data.secciones_df
+
+paises = data.paises_df
+
+
+
