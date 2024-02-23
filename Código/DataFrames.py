1d43e0b1f817692b32cb4ce8d45db5d9d3395ae1
Rocio Dominguez <132227093+rocio2403@users.noreply.github.com>
2024-02-22 21:54:23 -0300

    Update and rename tp_labo.py to DataFrames.py

--- Código/DataFrames.py
+++ Código/DataFrames.py
@@ -8,10 +8,11 @@ Fecha : Febrero 2024
 """
 import pandas as pd
 import Normalizar as n
+from inline_sql import sql, sql_val
 
 #Creamos DataFrames vacios correspondientes al Modelo Relacional de nuestro DER
 
-columnas_sede = ['id_sede', 'Nombre', 'Tipo', 'Estado', 'codigo_pais','Redes_Sociales']
+columnas_sede = ['id_sede', 'Nombre', 'Tipo', 'codigo_pais','URl_Red_Social']
 sedes_df = pd.DataFrame(columns=columnas_sede)
 
 
@@ -23,12 +24,12 @@ columnas_seccion = ['id_sede','Descripcion']
 secciones_df = pd.DataFrame(columns=columnas_seccion)
 
 
-columnas_redSocial=['id_sede','Nombre','Url']
+columnas_redSocial=['Nombre','Url']
 redSocial_df=pd.DataFrame(columns=columnas_redSocial)
 
 #Ahora importamos los datos ya limpios:
     
-carpeta = "../tablas_limpias/"
+carpeta = "/Users/Roju2/OneDrive/Desktop/Código/tablas limpias/"
 
 sedes = pd.read_csv(carpeta+'sedes.csv')
 PBI =pd.read_csv(carpeta + 'pbi.csv')
@@ -40,7 +41,7 @@ sedes_df['Nombre'] = sedes['sede_desc_ingles']
 sedes_df['id_sede']=sedes['sede_id']
 sedes_df['Tipo']=sedes['sede_tipo']
 sedes_df['codigo_pais']=sedes['pais_iso_3']
-sedes_df['Redes_Sociales']=sedes['redes_sociales']
+
 
 paises_df['Codigo'] = PBI['Country Code']
 paises_df['PBI'] = PBI['2022 PBI']
@@ -50,10 +51,30 @@ paises_df['Nivel_Ingreso'] = regiones['IncomeGroup']
 
 secciones_df['id_sede'] = secciones['sede_id']
 secciones_df['Descripcion'] = secciones['sede_desc_castellano']
-#Para normalizar, aplicamos una funcion importada de nuestro modulo 
+
+#Para importar los datos de la red social, hay que extraerlos. 
+
 redSocial_df['id_sede'] = sedes['sede_id']
 redSocial_df['Url'] = sedes['redes_sociales']
 
+#Como los atributos eran compuestos, llevamos a primera forma normal con una funcion de nuestro modulo.
+
 redSocial_df = n.acomodar(redSocial_df)
+#Para eliminar dependencias funcionales transitivas y llevar a 3FN, utilizamos consultas SQL para separar dependencias parciales y representar nuestro Modelo relacional
+sedes_df = sql^"""
+                    SELECT  s.Nombre, s.id_sede, s.Tipo, s.codigo_pais, rs.Url as URl_Red_Social
+                    FROM sedes_df as s
+                    INNER JOIN
+                    redSocial_df as rs
+                    ON s.id_sede = rs.id_sede
+
+ """
+redSocial_df = sql^"""
+                    SELECT Url, Nombre
+                    FROM redSocial_df
+"""
+
+
+
 
 
