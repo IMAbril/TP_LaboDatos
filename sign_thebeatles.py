affaa74ef684938dab3d2710ef86cde2eb220cd0
IMAbril <devabrilibarra@gmail.com>
2024-02-28 11:27:03 -0300

    Organización de archivos

---
+++ sign_thebeatles.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Trabajo Práctico N°2
+laboratorio de Datos - Verano 2024
+Autores : Ibarra, Abril; Vassolo, Francisco; Dominguez,Rocio
+Nombre del grupo: The Beatles 
+Fecha : Marzo 2024
+
+Contenido: --
+
+"""
+#%% =============================================================================
+# IMPORT módulos 
+# =============================================================================
+import pandas as pd
+
+#from modulos import modulo as  -> Se importa el archivo modulo guardado en la carpeta modulos
+
+
+#%% =============================================================================
+# Cargamos los datos
+# =============================================================================
+carpeta = './datasets/'
+sign_mnist = pd.read_csv(carpeta+'sign_mnist_train.csv')
+
+
+#%% =============================================================================
+# Funciones
+# =============================================================================
+
+
+#%% =============================================================================
+# Código global (o fuera de las funciones)
+# =============================================================================
