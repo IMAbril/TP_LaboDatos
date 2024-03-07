5addb812e1a1106892631458c3f27a70a15f067d
Rocio Dominguez <132227093+rocio2403@users.noreply.github.com>
2024-03-07 18:22:02 -0300

    Update KNN.py

--- modulos/KNN.py
+++ modulos/KNN.py
@@ -6,11 +6,18 @@ Autores : Ibarra, Abril; Vassolo, Francisco; Dominguez,Rocio
 Nombre del grupo: The Beatles 
 Fecha : Marzo 2024
 """
-from exploracion import sign
+from exploracion import sign, cantMuestras
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 
-#Trabajo Práctico N°2
 #A partir del dataframe original, construimos un nuevo dataframe que contenga sólo al subconjunto de imágenes correspondientes a señas de las letras L o A.
 
 data_L_A = sign[(sign['label'] == 0) | (sign['label'] == 11)]
+
+
+#Obtuvimos la cantidad de muestras de cada letra al realizar la exploración.
+cantidad_letra_A = cantMuestras[cantMuestras['letras'] == 'a']['cantidad'].values[0]
+print('Cantidad de muestras de la letra A:', cantidad_letra_A)
+
+cantidad_letra_L = cantMuestras[cantMuestras['letras'] == 'l']['cantidad'].values[0]
+print('Cantidad de muestras de la letra L:', cantidad_letra_L)
