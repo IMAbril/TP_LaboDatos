87eb9b1cb3ebc6d0673c1187abfc5283a59eb23c
Rocio Dominguez <132227093+rocio2403@users.noreply.github.com>
2024-03-08 10:48:48 -0300

    Subo codigo con KNN de columnas al azar

--- modulos/KNN.py
+++ modulos/KNN.py
@@ -26,3 +26,55 @@ print('Cantidad de muestras de la letra L:', cantidad_letra_L)
 diferenciaMuestral = cantidad_letra_A/cantidad_letra_L
 print('Las clases están balanceadas, pues al compararlas obtenemos un valor muy cercano a uno : ', round(diferenciaMuestral,2))
 
+#%% Ahora creamos nuestro clasificador y comenzamos a entrenarlo 
+
+# Comenzamos separando nuestras variables 
+X = data_L_A.drop('label', axis=1)  #Conservamos todas las columnas excepto 'label'
+y = data_L_A['label']
+
+#Separamos en un conjunto de entrenamiento y otro de validación. Colocamos una semilla, con random-state
+# Separamos en 80% entrenamiento y 20% prueba 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
+
+"""Ajustar un modelo de KNN considerando pocos atributos, por ejemplo
+3. Probar con distintos conjuntos de 3 atributos y comparar resultados.
+Analizar utilizando otras cantidades de atributos."""
+
+#Ahora iniaciamos un modelo KNN considerando todos los atributos y un numero de K = 3, pequeño
+neigh = KNeighborsClassifier(n_neighbors= 3)
+
+#Lo entenamos con los conjuntos de X e y
+
+neigh.fit(X,y)
+
+#Ahora evaluamos el modelo, calculando su R^2
+score = neigh.score(X, y)
+
+print("R^2 (train ): %.2f" % score)
+
+#######################################
+#COMENTARIO PARA INFORME
+#Al utilizar un numero K bajo, el modelo, se está sobreajustando
+
+
+#%%
+
+#Ahora Probamos distintos conjuntos de tres atributos (columnas) y comparamos resultados, evaluando en el conjunto de test
+#para ello, listamos todas las columnas de X, ya que no contiene 'label'
+
+lista_columnas = X.columns.tolist()
+
+# Separamos en conjuntos de train y test
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=5)
+
+
+k = 5
+
+# Probamos con distintos conjuntos de tres atributos y comparamos resultados
+for _ in range(5):   #Iteramos 5 veces
+    columnas_Al_Azar= random.sample(lista_columnas, 3) #seleccionamos columnas al azar
+    neigh = KNeighborsClassifier(n_neighbors=k) #iniciamos el modelo
+    neigh.fit(X_train[columnas_Al_Azar], y_train) #lo entrenamos con las columnas seleccionadas
+    score = neigh.score(X_test[columnas_Al_Azar], y_test)#lo evaluamos
+    print(f' Score del modelo: {score}')
+    
